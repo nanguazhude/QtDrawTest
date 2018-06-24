4f446e2fa8cabefe865bb594e6b20e95170b28a3
nanguazhude <nanguazhude@vip.qq.com>
2018-06-24 14:17:20 +0800

    +Test Code

--- Draw/main.cpp
+++ Draw/main.cpp
@@ -1,10 +1,46 @@
 ﻿#include "MainWindow.hpp"
 #include <QApplication>
 
-int main(int argc, char *argv[]){
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+#include <QtWidgets/QtWidgets>
+#include <QtGui/QtGui>
+#include <QtCore/QtCore>
 
-    return a.exec();
+class Test : public QWidget {
+public:
+
+	virtual void paintEvent(QPaintEvent *event) override {
+
+		QPainter varPainter{ this };
+
+		varPainter.setRenderHints(
+			QPainter::Antialiasing |
+			QPainter::TextAntialiasing |
+			QPainter::SmoothPixmapTransform |
+			QPainter::HighQualityAntialiasing
+		);
+
+		QStyle * varStyle = qApp->style();
+		QStyleOptionButton varDrawData;
+		varDrawData.rect = QRect{ 10,10,100,30 };
+		varDrawData.text = QStringLiteral("ABC");
+		varStyle->drawControl(QStyle::CE_PushButton, &varDrawData, &varPainter);
+
+		(void)event;
+		//https://blog.csdn.net/liang19890820/article/details/50974059
+		//QPainterPathStroker
+
+	}
+
+};
+
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
+
+	MainWindow window;
+	window.show();
+
+	Test test;
+	test.show();
+
+	return app.exec();
 }
