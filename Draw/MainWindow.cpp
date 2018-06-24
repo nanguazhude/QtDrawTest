87177b127b12bf28e502b01cba4f3d906710fa16
nanguazhude <nanguazhude@vip.qq.com>
2018-06-24 13:45:48 +0800

    +Draw

---
+++ Draw/MainWindow.cpp
@@ -0,0 +1,15 @@
+﻿#include "MainWindow.hpp"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow){
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow(){
+    delete ui;
+}
+
+
+
---
+++ Draw/MainWindow.hpp
@@ -0,0 +1,21 @@
+﻿#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_HPP
